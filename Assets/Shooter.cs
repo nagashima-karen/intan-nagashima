cb31c71529f58c2c034cae92dd25826feeda2e69
nagashima-karen <nagashima-karen@KYCNT1704-26.local>
2018-08-20 16:13:39 +0900

    (cs)シューターの追加

---
+++ Assets/Shooter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour 
+{
+    public GameObject BulletPrefab;
+    public float shotSpeed;
+    public float shotTorque;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetMouseButton(0)) Shot();	
+	}
+
+    public void Shot ()
+    {
+        //プレファブからBulletオブジェクトを生成
+        GameObject Bullet = (GameObject)Instantiate(
+        BulletPrefab,
+        transform.position,
+            Quaternion.identity
+        );
+        //BulletオブジェクトのRigidbodyを取得し力と回転を加える
+        Rigidbody BulletRigidBody = Bullet.GetComponent<Rigidbody>();
+        BulletRigidBody.AddForce(transform.forward * shotSpeed);
+    }
+}
