f3e323c3d6e24a2e857ab83125ed45571ac6a2af
nagashima-karen <nagashima-karen@KYCNT1704-26.local>
2018-08-21 15:41:10 +0900

    HPバーの実装(まだできていない)

---
+++ Assets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject HpBar1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        //  on damage
+        if (col.gameObject.tag == "Enemy")
+        {
+            HpBar1.gameObject.SendMessage("onDamage", 10);
+        }
+    }
+}
