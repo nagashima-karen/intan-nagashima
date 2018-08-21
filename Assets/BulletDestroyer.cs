311324e8682d361efa91cfd8dc6527ae587e37a7
nagashima-karen <nagashima-karen@KYCNT1704-26.local>
2018-08-21 14:40:49 +0900

    (cs)メッシュの付け直しとRigidBodyの解除

---
+++ Assets/BulletDestroyer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroyer : MonoBehaviour 
+{
+    void OnTriggerEnter(Collider other)
+    {
+
+        // もしもぶつかってきたオブジェクトのタグに「Bullet」という名前がついていたら
+        if (other.CompareTag("Bullet"))
+        {
+
+            // ぶつかってきたオブジェクトを破壊（削除）せよ。
+            Destroy(other.gameObject);
+        }
+    }
+  
+}
