fd84d9738bb838176352d2e3939e57e7cebf2028
nagashima-karen <nagashima-karen@KYCNT1704-26.local>
2018-08-23 18:55:01 +0900

    Good表示

--- Assets/Vitals.cs
+++ Assets/Vitals.cs
@@ -9,13 +9,32 @@ public class Vitals : MonoBehaviour
     private EnemyHP enemyHP;
     [SerializeField]
     private int damage;  //プレイヤーの攻撃力
- 
+    [SerializeField]
+    private GameObject text;
+
+    void Start()
+    {
+        text.GetComponent<TextMesh>().color = new Color(255, 255, 0, 0);
+    }
+
     void OnTriggerEnter(Collider hit)
     {
         if (hit.CompareTag("Bullet"))
         {
             enemyHP.HP -= damage; //攻撃で体力が減少
             Debug.Log("急所に当たった!!" + enemyHP);
+
+            StartCoroutine(Fadeout());
+
+        }
+    }
+
+    IEnumerator Fadeout()
+    {
+        for (float i = 0; i <= 20; i++)
+        {
+            text.GetComponent<TextMesh>().color = new Color(255, 255, 0, 1.0f - i / 20.0f);
+            yield return null;
         }
     }
 }
