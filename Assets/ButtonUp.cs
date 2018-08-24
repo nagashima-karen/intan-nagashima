c1635057e9dddf187376b43f3fa2e8b4601460eb
nagashima-karen <nagashima-karen@KYCNT1704-26.local>
2018-08-24 12:18:09 +0900

    (cs)ボタンをクリックして反応するところまで

---
+++ Assets/ButtonUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonUp : MonoBehaviour {
+    
+    [SerializeField]
+    private float speed = 10.0f;
+    [SerializeField]
+    private MoveGun GetMoveGun;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnClick()
+    {
+        if (transform.position.y >= 18.0f)
+            return;
+        transform.position += transform.up * speed * Time.deltaTime;
+        Debug.Log("Button click!");
+    }
+
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
